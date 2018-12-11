f1d66a974c33921253e3257eb24abed21663aa24
dme65 <dme65@cornell.edu>
2018-12-11 15:36:59 -0500

    Working on GPs with derivative observations.

--- gpytorch/kernels/__init__.py
+++ gpytorch/kernels/__init__.py
@@ -14,6 +14,7 @@ from .multitask_kernel import MultitaskKernel
 from .periodic_kernel import PeriodicKernel
 from .product_structure_kernel import ProductStructureKernel
 from .rbf_kernel import RBFKernel
+from .rbf_kernel_grad import RBFKernelGrad
 from .scale_kernel import ScaleKernel
 from .spectral_mixture_kernel import SpectralMixtureKernel
 from .white_noise_kernel import WhiteNoiseKernel
@@ -36,6 +37,7 @@ __all__ = [
     "ProductKernel",
     "ProductStructureKernel",
     "RBFKernel",
+    "RBFKernelGrad",
     "ScaleKernel",
     "SpectralMixtureKernel",
     "WhiteNoiseKernel",
